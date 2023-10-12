374c476fd2219cd2cf19591768c36d2d2daf5662
atakandll <atakanccnar@outlook.com>
2023-10-12 22:26:16 +0300

    UI Manager finalized

--- Assets/Scripts/Runtime/Managers/UIManager.cs
+++ Assets/Scripts/Runtime/Managers/UIManager.cs
@@ -1,9 +1,103 @@
-﻿using UnityEngine;
+﻿using System;
+using Runtime.Enums;
+using Runtime.Signals;
+using UnityEngine;
 
 namespace Runtime.Managers
 {
     public class UIManager : MonoBehaviour
     {
+        private void OnEnable()
+        {
+            SubscibeEvents();
+            OpenStartPanel();
+        }
+        private void SubscibeEvents()
+        {
+            CoreGameSignals.Instance.onLevelInitialize += OnLevelInitialize;
+            CoreGameSignals.Instance.onLevelFailed += OnLevelFailed;
+            CoreGameSignals.Instance.onLevelSuccessful += OnLevelSuccessful;
+            CoreGameSignals.Instance.onReset += OnReset;
+
+        }
+        private void OpenStartPanel()
+        {
+            CoreUISignals.Instance.onOpenPanel?.Invoke(UIPanelTypes.Start, 0);
+            CoreUISignals.Instance.onOpenPanel?.Invoke(UIPanelTypes.Level, 1);
+            CoreUISignals.Instance.onOpenPanel?.Invoke(UIPanelTypes.Shop, 2);
+            
+        }
+        private void OnLevelInitialize(byte levelValue)
+        {
+            CoreUISignals.Instance.onOpenPanel?.Invoke(UIPanelTypes.Start, 0);
+            CoreUISignals.Instance.onOpenPanel?.Invoke(UIPanelTypes.Level, 1);
+            CoreUISignals.Instance.onOpenPanel?.Invoke(UIPanelTypes.Shop, 2);
+            UISignals.Instance.onSetNewLevelValue?.Invoke(levelValue);
+        }
+
+        public void OnPlay()
+        {
+            CoreGameSignals.Instance.onPlay?.Invoke();
+            CoreUISignals.Instance.onClosePanel?.Invoke(0);
+            CoreUISignals.Instance.onClosePanel?.Invoke(2);
+            //CameraSignals.Instance.onChangeCameraState?.Invoke(CameraStates.Follow);
+        }
+        private void OnOpenWinPanel()
+        {
+            CoreUISignals.Instance.onOpenPanel?.Invoke(UIPanelTypes.Win, 2);
+        }
+        private void OnOpenFailPanel()
+        {
+            CoreUISignals.Instance.onOpenPanel?.Invoke(UIPanelTypes.Fail, 2);
+        }
+        public void OnNextLevel()
+        {
+            CoreGameSignals.Instance.onNextLevel?.Invoke();
+            CoreGameSignals.Instance.onReset?.Invoke();
+        }
+
+        public void OnRestartLevel()
+        {
+            CoreGameSignals.Instance.onRestartLevel?.Invoke();
+            CoreGameSignals.Instance.onReset?.Invoke();
+        }
+        private void OnLevelFailed()
+        {
+            OnOpenFailPanel();
+        }
         
+        private void OnLevelSuccessful()
+        {
+            OnOpenWinPanel();
+        }
+        public void OnIncomeUpdate()
+        {
+            UISignals.Instance.onClickIncome?.Invoke();
+            UISignals.Instance.onSetIncomeLvlText?.Invoke();
+        }
+
+        public void OnStackUpdate()
+        {
+            UISignals.Instance.onClickStack?.Invoke();
+            UISignals.Instance.onSetStackLvlText?.Invoke();
+        }
+        
+        private void UnsubscribeEvents()
+        {
+            CoreGameSignals.Instance.onLevelInitialize -= OnLevelInitialize;
+            CoreGameSignals.Instance.onLevelFailed -= OnLevelFailed;
+            CoreGameSignals.Instance.onLevelSuccessful -= OnLevelSuccessful;
+            CoreGameSignals.Instance.onReset -= OnReset;
+        }
+        private void OnDisable()
+        {
+            UnsubscribeEvents();
+        }
+        private void OnReset()
+        {
+            //CoreUISignals.Instance.onCloseAllPanel?.Invoke();
+        }
+
+       
     }
 }
--- Assets/Scripts/Runtime/Signals/CoreGameSignals.cs
+++ Assets/Scripts/Runtime/Signals/CoreGameSignals.cs
@@ -6,18 +6,20 @@ using UnityEngine.Events;
 namespace Runtime.Signals
 {
     public class CoreGameSignals : MonoSingleton<CoreGameSignals>
-    {
+    { 
         public UnityAction<GameStates> onChangeGameStates = delegate { };
-       public UnityAction<byte> onLevelInitialize = delegate(byte arg0) {  };
-       public UnityAction onClearActiveLevel = delegate() {  };
-       public UnityAction onNextLevel = delegate() {  };
-       public UnityAction onRestartLevel = delegate() {  };
-       public UnityAction onPlay = delegate { };
-       public UnityAction onReset = delegate { };
-       public Func<byte> onGetLevelID = delegate { return 0; };
+        public UnityAction<byte> onLevelInitialize = delegate(byte arg0) {  };
+        public UnityAction onClearActiveLevel = delegate() {  };
+        public UnityAction onLevelSuccessful = delegate { };
+        public UnityAction onLevelFailed = delegate { };
+        public UnityAction onNextLevel = delegate() {  };
+        public UnityAction onRestartLevel = delegate() {  };
+        public UnityAction onPlay = delegate { };
+        public UnityAction onReset = delegate { };
+        public Func<byte> onGetLevelID = delegate { return 0; };
        
-       public Func<byte> onGetIncomeLevel = delegate { return 0; };
-       public Func<byte> onGetStackLevel = delegate { return 0; };
+        public Func<byte> onGetIncomeLevel = delegate { return 0; };
+        public Func<byte> onGetStackLevel = delegate { return 0; };
 
        
     }
