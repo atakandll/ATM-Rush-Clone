bb7a92524d80c149d606d27c7d67212e9df44503
atakandll <atakanccnar@outlook.com>
2023-10-28 04:05:49 +0300

    Mini Game System

---
+++ Assets/Scripts/Runtime/Controllers/MiniGame/WallCheckController.cs
@@ -0,0 +1,48 @@
+﻿using DG.Tweening;
+using Runtime.Managers;
+using UnityEngine;
+
+namespace Runtime.Controllers.MiniGame
+{
+    public class WallCheckController : MonoBehaviour
+    {
+        #region Self Variables
+
+        #region SerializeField Variables
+
+        [SerializeField] private MiniGameManager manager;
+
+        #endregion
+
+        #region Private Variables
+
+        private float _changesColor;
+        private float _multiplier;
+
+        private readonly string _wall = "Wall";
+
+        #endregion
+
+        #endregion
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.CompareTag(_wall)) return;
+            _multiplier += 0.1f;
+            manager.SetMultiplier(_multiplier);
+            ChangeColor(other);
+        }
+
+        private void ChangeColor(Collider other)
+        {
+            _changesColor = (0.036f + _changesColor) % 1;
+            var otherGameObject = other.gameObject;
+            otherGameObject.GetComponent<Renderer>().material.DOColor(Color.HSVToRGB(_changesColor, 1, 1), 0.1f);
+            otherGameObject.transform.DOLocalMoveZ(-3, 0.1f);
+        }
+        public void OnReset()
+        {
+            _changesColor = 0;
+            _multiplier = 0.98f;
+        }
+    }
+}
---
+++ Assets/Scripts/Runtime/Managers/MiniGameManager.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections;
+using DG.Tweening;
+using Runtime.Controllers.MiniGame;
+using Runtime.Signals;
+using TMPro;
+using UnityEngine;
+
+namespace Runtime.Managers
+{
+    public class MiniGameManager : MonoBehaviour
+    {
+        #region Self Variables
+
+        #region Serialized Variables
+
+        [SerializeField] private GameObject wallObject;
+        [SerializeField] private GameObject fakeMoneyObject;
+        [SerializeField] private Transform fakePlayer;
+        [SerializeField] private Material mat;
+        
+        [SerializeField] private short wallCount, fakeMoneyCount;
+        
+        [SerializeField] private WallCheckController wallCheckController;
+
+        #endregion
+        
+        #region Private Veriables
+
+        private int _score;
+        private float _multiplier;
+        private Vector3 _initializePos;
+
+        #endregion
+        
+        
+        #endregion
+
+        private void OnEnable()
+        {
+            SubscribeEvents();
+        }
+
+        private void SubscribeEvents()
+        {
+            ScoreSignals.Instance.onSendFinalScore += OnSendScore;
+            ScoreSignals.Instance.onGetMultiplier += OnGetMultiplier;
+            CoreGameSignals.Instance.onMiniGameStart += OnMiniGameStart;
+            CoreGameSignals.Instance.onReset += OnReset;
+        }
+        
+        private void OnMiniGameStart()
+        {
+            fakePlayer.gameObject.SetActive(true);
+            StartCoroutine(GoUp());
+        }
+
+        private IEnumerator GoUp()
+        {
+            yield return new WaitForSeconds(1f);
+            if (_score == 0)
+            {
+                CoreGameSignals.Instance.onLevelFailed?.Invoke();
+            }
+            else
+            {
+                fakePlayer.DOLocalMoveY(Mathf.Clamp(_score, 0, 900), 2.7f).SetEase(Ease.Flash).SetDelay(1f);
+                yield return new WaitForSeconds(4.5f);
+                CoreGameSignals.Instance.onLevelSuccessful?.Invoke();
+            }
+        }
+        internal void SetMultiplier(float multiplierValue)
+        {
+            _multiplier = multiplierValue;
+        }
+
+        private float OnGetMultiplier()
+        {
+            return _multiplier;
+        }
+
+        private void OnSendScore(int scoreValue)
+        {
+           _score = scoreValue;
+        }
+
+        private void UnSubscribeEvents()
+        {
+            ScoreSignals.Instance.onSendFinalScore -= OnSendScore;
+            ScoreSignals.Instance.onGetMultiplier -= OnGetMultiplier;
+            CoreGameSignals.Instance.onMiniGameStart -= OnMiniGameStart;
+            CoreGameSignals.Instance.onReset -= OnReset;
+        }
+
+        private void OnDisable()
+        {
+            UnSubscribeEvents();
+        }
+
+       
+
+        private void Start()
+        {
+            SpawnWallObjects();
+            SpawnFakeMoneyObjects();
+            Init();
+        }
+
+        private void Init()
+        {
+            _initializePos = fakePlayer.localPosition;
+        }
+
+        private void SpawnWallObjects()
+        {
+            for (int i = 0; i < wallCount; i++)
+            {
+                var ob = Instantiate(wallObject, transform);
+                ob.transform.localPosition = new Vector3(0, i * 30, 0);
+                ob.transform.GetChild(0).GetComponent<TextMeshPro>().text = "x" + ((i / 10f) + 1f);
+
+            }
+        }
+
+        private void SpawnFakeMoneyObjects()
+        {
+            for (int i = 0; i < fakeMoneyCount; i++)
+            {
+                var ob = Instantiate(fakeMoneyObject, fakePlayer);
+                ob.transform.localPosition = new Vector3(0, -1 * 1.58f, -7);
+
+            }
+        }
+
+        private void ResetWalls()
+        {
+            for (int i = 1; i < wallCount; i++)
+            {
+                transform.GetChild(i).GetComponent<Renderer>().material = mat;
+                transform.GetChild(i).transform.position = Vector3.zero;
+            }
+        }
+        private void OnReset()
+        {
+            StopAllCoroutines();
+            DOTween.KillAll();
+            ResetWalls();
+            ResetFakePlayer();
+        }
+
+        private void ResetFakePlayer()
+        {
+            fakePlayer.gameObject.SetActive(false);
+            fakePlayer.localPosition = _initializePos;
+            wallCheckController.OnReset();
+
+        }
+    }
+}
